50ba9722f308533003c64b0c24882c75134dd769
KuanWei <KuanWei@DESKTOP-8R38C1M>
2021-03-31 00:05:09 +0800

    (resend commitment) add EditorSignShowBehavior and set color to show EditorSign

---
+++ tetris/src/com/editor/sign/control/editorsign/EditorSignShowBehavior.java
@@ -0,0 +1,32 @@
+package com.editor.sign.control.editorsign;
+
+import java.awt.Color;
+
+import com.editor.sign.control.Behavior;
+import com.model.Sign;
+import com.tool.Direction;
+import com.tool.TColor;
+import com.view.EditorSign;
+
+public class EditorSignShowBehavior extends Behavior {
+
+	@Override
+	public void run() {
+		EditorSign editorsign = (EditorSign) this.getRequest("editorsign");
+		Sign sign = (Sign) this.getRequest("sign");
+		Direction d = editorsign.getDirection();
+		if (sign.containLocalPoint(d.getX(), d.getY())) {
+			editorsign.getMainPanel().setBackground(Color.red);
+		} else {
+			editorsign.setBackground(TColor.baseColor);
+		}
+		
+		if(sign.isPivot(d.getX(), d.getY())) {
+			editorsign.setBackground(Color.cyan);
+		}else {
+			editorsign.setBackground(TColor.baseColor);
+		}
+
+	}
+
+}
---
+++ tetris/src/com/tool/TColor.java
@@ -0,0 +1,7 @@
+package com.tool;
+
+import java.awt.Color;
+
+public class TColor {
+	public static Color baseColor=new Color(240,240,240);
+}
