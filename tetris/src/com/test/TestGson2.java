69f77da6a00bbde8c361260e62abfb5d03cf9e2f
KuanWei <KuanWei@DESKTOP-8R38C1M>
2021-04-05 23:49:18 +0800

    testGson2 add nest serializer class Name

--- tetris/src/com/test/TestGson2.java
+++ tetris/src/com/test/TestGson2.java
@@ -1,4 +1,5 @@
 package com.test;
+
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,27 +14,77 @@ import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import com.google.gson.reflect.TypeToken;
+import com.test.Birds.FullName;
+import com.test.Birds.Name;
 
 abstract class Birds {
-	public final static String NameInfoType_Master = "master";
-	public final static String NameInfoType_Self = "self";
+
+	protected static class Name {
+		private String name;
+
+		public Name(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String firstName) {
+			name = firstName;
+		}
+
+		public String toString() {
+			return String.format("%s", this.name);
+		}
+	}
+
+	protected static class FullName extends Name {
+		private String lastName;
+
+		public FullName(String firstName, String lastName) {
+			super(firstName);
+			this.lastName = lastName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public void setFirstName(String firstName) {
+			super.setName(firstName);
+		}
+
+		public String getFirstName() {
+			return super.getName();
+		}
+
+		public String toString() {
+			return String.format("%s %s", this.getFirstName(), this.lastName);
+		}
+
+	}
 
 	protected enum NameInfoType {
 		Master, Self
 	}
 
-	private Map<Enum<NameInfoType>, String> mapNameInfo = new HashMap<>();// <type,name>
+	private Map<Enum<NameInfoType>, Name> mapNameInfo = new HashMap<>();// <type,name>
 
-	public void setNameInfo(String name, NameInfoType type) {
+	public void setNameInfo(Name name, NameInfoType type) {
 		this.mapNameInfo.put(type, name);
 	}
 
-	public String getName(NameInfoType type) {
+	public Name getName(NameInfoType type) {
 		return mapNameInfo.get(type);
 	}
 
 	public String toString() {
-		return String.format("%s::infoMap->%s", this.getClass().getClass().getSimpleName() , this.mapNameInfo);
+		return String.format("%s::infoMap->%s", this.getClass().getClass().getSimpleName(), this.mapNameInfo);
 	}
 
 }
@@ -83,14 +134,14 @@ public class TestGson2 {
 
 	public static void main(String[] args) {
 		Eagle eagle = new Eagle();
-		eagle.setNameInfo("Alice", Birds.NameInfoType.Self);
-		eagle.setNameInfo("Peter", Birds.NameInfoType.Master);
+		eagle.setNameInfo(new Name("Alice"), Birds.NameInfoType.Self);
+		eagle.setNameInfo(new FullName("Peter", "Wu"), Birds.NameInfoType.Master);
 		eagle.setFlyHeight(100);
 		eagle.setHuntingAtk(3600);
 
 		Penguin penguin = new Penguin();
-		penguin.setNameInfo("Tiffany", Birds.NameInfoType.Self);
-		penguin.setNameInfo("Mary", Birds.NameInfoType.Master);
+		penguin.setNameInfo(new Name("Tiffany"), Birds.NameInfoType.Self);
+		penguin.setNameInfo(new FullName("Mary", "Li"), Birds.NameInfoType.Master);
 		penguin.setSwimmingSpeed(20);
 
 		Map<String, Birds> toJsonPetMap = new HashMap<>();// <master,pet>
@@ -104,17 +155,28 @@ public class TestGson2 {
 	}
 
 	static String toJson(Map<String, Birds> mapPet) {
-		JsonSerializer<Birds> serializer = new JsonSerializer<Birds>() {
+		JsonSerializer<Birds> birdsSerializer = new JsonSerializer<Birds>() {
 			@Override
 			public JsonElement serialize(Birds birds, Type typeOfOri, JsonSerializationContext context) {
+				JsonSerializer<Name> nameSerializer = new JsonSerializer<Name>() {
+					@Override
+					public JsonElement serialize(Name name, Type typeOfOri, JsonSerializationContext context) {
+						JsonObject jsonObj = new JsonObject();
+						jsonObj.addProperty("type", name.getClass().getSimpleName());
+						jsonObj.add("data", new Gson().toJsonTree(name));
+						return jsonObj;
+					}
+				};
 				JsonObject jsonObj = new JsonObject();
 				jsonObj.addProperty("type", birds.getClass().getSimpleName());
-				jsonObj.add("data", new Gson().toJsonTree(birds));
+				jsonObj.add("data", new GsonBuilder().registerTypeAdapter(Name.class, nameSerializer).create() .toJsonTree(birds));
 				return jsonObj;
 			}
 		};
-		String strJson = new GsonBuilder().registerTypeAdapter(Eagle.class, serializer)
-				.registerTypeAdapter(Penguin.class, serializer).create().toJson(mapPet);
+
+		String strJson = new GsonBuilder() .registerTypeAdapter(Eagle.class, birdsSerializer)
+				.registerTypeAdapter(Penguin.class, birdsSerializer)
+				.create().toJson(mapPet);
 		return strJson;
 	}
 
@@ -134,6 +196,21 @@ public class TestGson2 {
 					public Enum<?> deserialize(JsonElement elem, Type typeOfOri, JsonDeserializationContext context)
 							throws JsonParseException {
 						return new Gson().fromJson(elem, Birds.NameInfoType.class);
+					}
+				}).registerTypeAdapter(Name.class, new JsonDeserializer<Name>() {
+					@Override
+					public Name deserialize(JsonElement elem, Type typeOfOri, JsonDeserializationContext context)
+							throws JsonParseException {
+						String type = elem.getAsJsonObject().get("type").getAsString();
+						JsonObject data = elem.getAsJsonObject().get("data").getAsJsonObject();
+						switch (type) {
+						case "Name":
+							return new Gson().fromJson(data, Name.class);
+						case "FullName":
+							return new Gson().fromJson(data, FullName.class);
+						default:
+							throw new IllegalArgumentException("No match class");
+						}
 					}
 				}).create();
 				switch (type) {
